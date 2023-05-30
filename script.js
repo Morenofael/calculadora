7b3e55517ffc9800c16c17e8075da51aa7b5429d
Rafael Checchia Moreno <rafaelchecchia@proton.me>
2023-05-29 21:40:27 -0300

    calculadora super pica ultra foda 1.0

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const substract = (numA,numB) => numA-numB;
 const multiply = (numA,numB) => numA*numB;
 const divide = (numA,numB) => numA/numB;
 const operate = function(numA,operator,numB){
-    if(operator=="+") return add(numA,numB);
+    if(operator=="+") return add(parseFloat(numA),parseFloat(numB));
     else if(operator=="-") return substract(numA,numB);
     else if(operator=="*") return multiply(numA,numB);
     else if(operator=="/") return divide(numA,numB);
@@ -14,17 +14,47 @@ let numB;
 let displayValue = "";
 
 function addNumberToDisplay(num){
+    if(num == "."){
+        if(displayValue.indexOf(num) >= 0){
+            num = "";
+        }
+    }
     displayValue+=num;
     display.textContent = displayValue;
+    removeErrorMessage();
 }
 function setOperator(op){
+    if(operator != undefined) makeOperation();
     operator = op;
     numA = displayValue;
-    clearDisplay();
+    displayValue = "";
+    display.textContent = displayValue;
 }
 function clearDisplay(){
     displayValue = "";
     display.textContent = displayValue;
+    numA = 0;
+    numB = 0;
+    operator = undefined;
+}
+function makeOperation(){
+    numB = displayValue;
+    displayValue = operate(numA, operator, numB);
+    numA = displayValue;
+    display.textContent = displayValue;
+    if(displayValue == Infinity) appendErrorMessage();
+}
+function appendErrorMessage(){
+    clearDisplay();
+    const body = document.querySelector("body");
+    const errorMessage = document.createElement("p");
+    errorMessage.textContent = "vc não pode dividor por 0, bobinho";
+    console.log(body.lastChild)
+    body.appendChild(errorMessage);
+}
+function removeErrorMessage(){
+    const body = document.querySelector("body");
+    if(body.lastChild.tagName == "P") body.removeChild(body.lastChild);
 }
 const display = document.getElementById("display");
 const nums = document.querySelectorAll(".num");
@@ -33,3 +63,5 @@ const ops = document.querySelectorAll(".op");
 ops.forEach(op => op.addEventListener("click",()=>setOperator(op.textContent)));
 const ce = document.querySelector(".clear");
 ce.addEventListener("click",()=>clearDisplay());
+const equals = document.querySelector(".equals");
+equals.addEventListener("click",()=>makeOperation());
